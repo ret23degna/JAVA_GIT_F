b2a8529fa55a8b659376f22a7ce5fb99420dc8a1
SEGA <ret23degna@gmail.com>
2024-07-18 21:01:51 +0500

    first commit

---
+++ src/test/java/JabaTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class JabaTest {
+    @Test
+    void ShouldOpen() {
+        String date = "28.11.2020";
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate parsedDate = LocalDate.parse(date, formater);
+        Assertions.assertEquals(DayOfWeek.SATURDAY, parsedDate.getDayOfWeek());
+    }
+}
