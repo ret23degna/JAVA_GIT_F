c83e9aabb372afb58a69896815bc53a9187d6d78
SEGA <ret23degna@gmail.com>
2024-07-18 21:04:42 +0500

    first commit

--- src/test/java/JabaTest.java
+++ src/test/java/JabaTest.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 public class JabaTest {
     @Test
     void ShouldOpen() {
-        String date = "28.11.2020";
+        String date = "28.11.2020";//123
         DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate parsedDate = LocalDate.parse(date, formater);
         Assertions.assertEquals(DayOfWeek.SATURDAY, parsedDate.getDayOfWeek());
